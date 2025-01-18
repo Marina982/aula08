9671a17a3ec976af5c3b492b8385a2536dde36dd
Marina982 <159331002+Marina982@users.noreply.github.com>
2025-01-18 20:11:45 -0300

    save

--- cliente/src/components/Footer.jsx
+++ cliente/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css';
 export default function Footer() {
   return (
     <header className={styles.footer}>
-      <p className={styles.title}>Desenvolvedor: Marina /**/ e Olavo /**/</p>
+      <p className={styles.title}>Desenvolvedor: Marina Goes e Olavo Goes</p>
       <img
       />
     </header>
--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
-import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import { Button, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
